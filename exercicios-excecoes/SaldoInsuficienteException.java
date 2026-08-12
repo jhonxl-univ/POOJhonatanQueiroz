8542c0d60c2baa99960aac607480e99fdb7c69e7
jhonxl-univ <jhonatan.queiroz@soulasalle.com.br>
2026-08-12 20:51:12 -0300

    Update SaldoInsuficienteException.java

--- exercicios-excecoes/SaldoInsuficienteException.java
+++ exercicios-excecoes/SaldoInsuficienteException.java
@@ -1,9 +1,4 @@
-// Jhonatan Carlos Rodrigues Queiroz
 
-/**
- * Exceção personalizada lançada quando se tenta sacar um valor maior
- * que o saldo disponível em uma ContaBancaria.
- */
 public class SaldoInsuficienteException extends Exception {
 
     public SaldoInsuficienteException(String mensagem) {
