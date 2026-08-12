ad98b28a7775fb576088ebfbc52a2318dab16975
jhonxl-univ <jhonatan.queiroz@soulasalle.com.br>
2026-08-12 20:50:57 -0300

    Update IdadeInvalidaException.java

--- exercicios-excecoes/IdadeInvalidaException.java
+++ exercicios-excecoes/IdadeInvalidaException.java
@@ -1,9 +1,5 @@
 // Jhonatan Carlos Rodrigues Queiroz
 
-/**
- * Exceção personalizada lançada quando a idade informada é inválida
- * (menor que 0 ou maior que 150).
- */
 public class IdadeInvalidaException extends RuntimeException {
 
     public IdadeInvalidaException(String mensagem) {
