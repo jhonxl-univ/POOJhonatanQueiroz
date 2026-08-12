4b17c740575ae2aa14268b5ab79cd5037883156f
jhonxl-univ <jhonatan.queiroz@soulasalle.com.br>
2026-08-12 20:52:19 -0300

    Update ContaBancaria.java

--- exercicios-excecoes/ContaBancaria.java
+++ exercicios-excecoes/ContaBancaria.java
@@ -1,10 +1,4 @@
-// Jhonatan Carlos Rodrigues Queiroz
 
-/**
- * Classe que representa uma conta bancária simples, com saldo e
- * operação de saque que lança SaldoInsuficienteException quando o
- * valor solicitado é maior que o saldo disponível.
- */
 public class ContaBancaria {
 
     private double saldo;
